43263d0e928652c1f5318b635bf444d327f3287a
Arnold Gandarillas Castillo <arkgast@gmail.com>
2017-08-15 23:50:06 -0400

    Improve webpack output management

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const App = () => (
-  <div>
-    <h1>React Firebase Starter</h1>
-  </div>
-)
-
 render(
   <App />,
   document.getElementById('app')
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Firebase React Starter',
+      template: 'src/index.template.html',
+      inject: true,
+      hash: true,
+      minify: {
+        caseSensitive: true,
+        collapseWhitespace: true
+      },
+      chunkSortMode: 'dependency'
+    }),
+    new CleanWebpackPlugin(['public'])
+  ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public/js')
+    filename: 'js/[name].bundle.js',
+    path: path.resolve(__dirname, 'public')
   },
   module: {
     rules: [
