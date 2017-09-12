de470f178e4c72a4a34072622ad285232780e913
Arnold Gandarillas Castillo <arkgast@gmail.com>
2017-09-12 02:26:45 -0400

    Improve firebase performance
    The result is the reduction of the vendor file in almost 50% of its size.

---
+++ src/services/firebase/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyCZY96yRLWhsDcuTFnmgLfHoSQnyU2xCIk',
+  authDomain: 'react-firebase-starter-d0953.firebaseapp.com',
+  databaseURL: 'https://react-firebase-starter-d0953.firebaseio.com',
+  projectId: 'react-firebase-starter-d0953',
+  storageBucket: 'react-firebase-starter-d0953.appspot.com',
+  messagingSenderId: '521312521561'
+}
--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,16 +1,9 @@
-import Firebase from 'firebase'
+import Firebase from 'firebase/app'
+import 'firebase/auth'
 
 import Auth from './auth'
+import config from './config'
 
-
-const config = {
-  apiKey: 'AIzaSyCZY96yRLWhsDcuTFnmgLfHoSQnyU2xCIk',
-  authDomain: 'react-firebase-starter-d0953.firebaseapp.com',
-  databaseURL: 'https://react-firebase-starter-d0953.firebaseio.com',
-  projectId: 'react-firebase-starter-d0953',
-  storageBucket: 'react-firebase-starter-d0953.appspot.com',
-  messagingSenderId: '521312521561'
-}
 
 Firebase.initializeApp(config)
 
